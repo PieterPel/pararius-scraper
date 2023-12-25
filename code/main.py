355af30c3e9eb8ca9b01cf6855009779c416b8ac
Pieter Pel <pelpieter@gmail.com>
2023-12-25 10:13:22 +0100

    Added txt files with previously scanned urls, to speed up process

--- code/main.py
+++ code/main.py
@@ -1,4 +1,3 @@
-
 import methods
 import openpyxl
 import pickle
@@ -9,7 +8,6 @@ import sys
 cities = ["Rotterdam", "Amsterdam"]
 
 for city in cities:
-
     # Open the workbook
     os.chdir(config.folder_location)
     wb = openpyxl.load_workbook("output/pararius.xlsx")
@@ -17,27 +15,61 @@ for city in cities:
     # Extract the urls of the city
     urls = methods.get_all_listing_urls(city.lower())
 
+    # Load the extracted urls from the previous time
+    with open(f"output/txt_files/{city}_urls.txt", "r") as f:
+        old_urls = [line for line in f]
+
+    # Save the new urls
+    with open(f"output/txt_files/{city}_urls.txt", "w") as f:
+        for url in urls:
+            f.write(url + "\n")
+
+    # Get the new urls
+    new_urls = [url for url in urls if url not in old_urls]
+
+    # Get the removed urls
+    removed_urls = list(set(old_urls) - set(urls))
+
     # Extract the scraped dictionary of the city
-    scraped_dict = methods.get_scraped_dict(urls)
+    scraped_dict = methods.get_scraped_dict(new_urls)
+
+    # Load the pickle that has the dictionary of the old urls
+    with open(f"output/pickles/{city}_all.pkl", "rb") as fp:
+        old_dict = pickle.load(fp)
+
+    # Remove the removed urls
+    for code, dict in old_dict.items():
+        if dict["url"] in removed_urls:
+            del old_dict[code]
+
+    # Combine the scraped dictionaries
+    scraped_dict.update(old_dict)
 
     # Put all the listings in a sheet
     ws_all = wb[city + " All"]
-    methods.put_dictionary_in_sheet(methods.order, methods.var_display_dict, scraped_dict, ws_all)
+    methods.put_dictionary_in_sheet(
+        methods.order, methods.var_display_dict, scraped_dict, ws_all
+    )
 
     # Put the filtered listings in a sheet
     ws_filtered = wb[city + " Filtered"]
-    filtered_dict = {key: value for key, value in scraped_dict.items() if methods.filter_listings(key, value, city)}
-    methods.put_dictionary_in_sheet(methods.order, methods.var_display_dict, filtered_dict, ws_filtered)
+    filtered_dict = {
+        key: value
+        for key, value in scraped_dict.items()
+        if methods.filter_listings(key, value, city)
+    }
+    methods.put_dictionary_in_sheet(
+        methods.order, methods.var_display_dict, filtered_dict, ws_filtered
+    )
 
     # Save dictionaries using pickle
-    with open(f'output/pickles/{city}_all.pkl', 'wb') as fp:
+    with open(f"output/pickles/{city}_all.pkl", "wb") as fp:
         pickle.dump(scraped_dict, fp)
 
-    with open(f'output/pickles/{city}_filtered.pkl', 'wb') as fp:
+    with open(f"output/pickles/{city}_filtered.pkl", "wb") as fp:
         pickle.dump(filtered_dict, fp)
 
     # Save workbook
-    wb.save('output/pararius.xlsx')
+    wb.save("output/pararius.xlsx")
 
 sys.exit(0)
-
--- code/methods.py
+++ code/methods.py
@@ -1,7 +1,7 @@
-
 # Import packages
 import requests
-from bs4 import BeautifulSoup as bs 
+from bs4 import BeautifulSoup as bs
+
 
 # Method that returns the number of pages of a base url
 def get_number_of_pages(base_url):
@@ -13,24 +13,26 @@ def get_number_of_pages(base_url):
     numpages = get_number(numpages_html[-2].text)
     return numpages
 
+
 # Method that returns that only returns the simple digits given a string
 def get_number(string):
     allowed = [str(i) for i in range(10)]
     str_numbers = list(filter(lambda x: x in allowed, [*string]))
-    joined = ''.join(str_numbers)
+    joined = "".join(str_numbers)
     value = int(joined)
     return value
 
+
 # Method that returns all the rental listings urls of a given city
 def get_all_listing_urls(city):
+    print("Beginning to extract the listing urls")
 
-    print('Beginning to extract the listing urls')
-
-    numpages = get_number_of_pages(f"https://www.pararius.nl/huurwoningen/{city.lower()}")
+    numpages = get_number_of_pages(
+        f"https://www.pararius.nl/huurwoningen/{city.lower()}"
+    )
 
     listing_urls = []
-    for i in range(1, numpages+1):
-    
+    for i in range(1, numpages + 1):
         # Obtain link and soup of page
         page_url = f"https://www.pararius.nl/huurwoningen/{city.lower()}/page-{str(i)}"
 
@@ -38,8 +40,16 @@ def get_all_listing_urls(city):
         s = bs(req.text, "html.parser")
 
         # Obtain links of listings on page
-        listings_on_page_html = s.find_all("a", {"class": "listing-search-item__link listing-search-item__link--title"})
-        listing_on_page_urls = ["https://www.pararius.nl" + link.get("href") for link in listings_on_page_html]
+        listings_on_page_html = s.find_all(
+            "a",
+            {
+                "class": "listing-search-item__link listing-search-item__link--title"
+            },
+        )
+        listing_on_page_urls = [
+            "https://www.pararius.nl" + link.get("href")
+            for link in listings_on_page_html
+        ]
 
         # Add listings of page to list
         listing_urls += listing_on_page_urls
@@ -49,28 +59,62 @@ def get_all_listing_urls(city):
 
 # Method that returns a scraped dictionary given urls of listings
 def get_scraped_dict(urls):
-
-    print('''You can expect error messages: not all listings have all information,
+    print(
+        """You can expect error messages: not all listings have all information,
           especially regarding the furnishing and number of bathrooms.
-          ''')
-    
+          """
+    )
+
     # Dictionary that contains the variables that are found in the same way by Beautiful Soup, the last entry is a list containing the variables that are numbers
-    simple_extractions = {"price": ["div", {"class": "listing-detail-summary__price"}],
-                      "area": ["li", {"class": "illustrated-features__item illustrated-features__item--surface-area"}],
-                      "nrooms": ["li", {"class": "illustrated-features__item illustrated-features__item--number-of-rooms"}],
-                      "nbedrooms": ["dd", {"class": "listing-features__description listing-features__description--number_of_bedrooms"}],
-                      "nbathrooms": ["dd", {"class": "listing-features__description listing-features__description--number_of_bathrooms"}],
-                      "furnished": ["li", {"class": "illustrated-features__item illustrated-features__item--interior"}],
-                      "agent": ["a", {"class": "agent-summary__title-link"}],
-                      "status": ["dd", {"class": "listing-features__description listing-features__description--status"}],
-                      "description": ["div", {"class": "listing-detail-description__content"}],
-                      "numbers": ["price", "area", "nrooms", "nbedrooms", "nbathrooms"]
-                    }
+    simple_extractions = {
+        "price": ["div", {"class": "listing-detail-summary__price"}],
+        "area": [
+            "li",
+            {
+                "class": "illustrated-features__item illustrated-features__item--surface-area"
+            },
+        ],
+        "nrooms": [
+            "li",
+            {
+                "class": "illustrated-features__item illustrated-features__item--number-of-rooms"
+            },
+        ],
+        "nbedrooms": [
+            "dd",
+            {
+                "class": "listing-features__description listing-features__description--number_of_bedrooms"
+            },
+        ],
+        "nbathrooms": [
+            "dd",
+            {
+                "class": "listing-features__description listing-features__description--number_of_bathrooms"
+            },
+        ],
+        "furnished": [
+            "li",
+            {
+                "class": "illustrated-features__item illustrated-features__item--interior"
+            },
+        ],
+        "agent": ["a", {"class": "agent-summary__title-link"}],
+        "status": [
+            "dd",
+            {
+                "class": "listing-features__description listing-features__description--status"
+            },
+        ],
+        "description": [
+            "div",
+            {"class": "listing-detail-description__content"},
+        ],
+        "numbers": ["price", "area", "nrooms", "nbedrooms", "nbathrooms"],
+    }
 
     scraped_dict = {}
 
     for url in urls:
-
         listing_dict = {}
 
         try:
@@ -78,7 +122,7 @@ def get_scraped_dict(urls):
             listing_dict["url"] = url
 
             # Code
-            code = url.split('/')[-2]
+            code = url.split("/")[-2]
             listing_dict["code"] = code
 
             # Obtain html
@@ -87,8 +131,8 @@ def get_scraped_dict(urls):
 
             # Extract all simple elements
             for key, value in simple_extractions.items():
-
-                if key == "numbers": continue
+                if key == "numbers":
+                    continue
 
                 try:
                     item_html = soup.find(value[0], value[1])
@@ -99,22 +143,30 @@ def get_scraped_dict(urls):
                     listing_dict[key] = item
                 except:
                     listing_dict[key] = "NA"
-                    print(f"Something went wrong with listing {url} when extracting {key}")
+                    print(
+                        f"Something went wrong with listing {url} when extracting {key}"
+                    )
 
             # Edit description
-            listing_dict["description"] = listing_dict["description"].replace('\n', ' ')       
+            listing_dict["description"] = listing_dict["description"].replace(
+                "\n", " "
+            )
 
             # Price per bedroom
-            price_per_bedroom = round(listing_dict["price"] / listing_dict["nbedrooms"])
+            price_per_bedroom = round(
+                listing_dict["price"] / listing_dict["nbedrooms"]
+            )
             listing_dict["price_per_bedroom"] = price_per_bedroom
 
             # Neighbourhood
             # Zipcode
-            location_html = soup.find("div", {"class": "listing-detail-summary__location"})
+            location_html = soup.find(
+                "div", {"class": "listing-detail-summary__location"}
+            )
             location_split = location_html.text.split()
             zipcode = location_split[0] + location_split[1]
-            neighborhood = ' '.join(location_split[2:])
-            neighborhood = neighborhood.replace('(','').replace(')','')
+            neighborhood = " ".join(location_split[2:])
+            neighborhood = neighborhood.replace("(", "").replace(")", "")
             listing_dict["zipcode"] = zipcode
             listing_dict["neighborhood"] = neighborhood
 
@@ -124,14 +176,21 @@ def get_scraped_dict(urls):
             listing_dict["street"] = street
 
             # Offered since
-            since_html = soup.find("dd", {"class": "listing-features__description listing-features__description--offered_since"})
-            since_down_html = since_html.find("span", {"class": "listing-features__main-description"})
+            since_html = soup.find(
+                "dd",
+                {
+                    "class": "listing-features__description listing-features__description--offered_since"
+                },
+            )
+            since_down_html = since_html.find(
+                "span", {"class": "listing-features__main-description"}
+            )
             since = since_down_html.text
             listing_dict["since"] = since
 
             # Added to parent dictionary
-            scraped_dict[code]= listing_dict
-            
+            scraped_dict[code] = listing_dict
+
         except Exception as e:
             print(f"something went wrong with {url}")
             print(e)
@@ -141,70 +200,84 @@ def get_scraped_dict(urls):
 
 # Method that puts a dictionary in a sheet
 def put_dictionary_in_sheet(order, var_display_dict, scraped_dict, sheet):
-
     # Give the columns names
     for i in range(len(order)):
         var = order[i]
-        sheet.cell(row=1, column = i+1, value = var_display_dict[var])
+        sheet.cell(row=1, column=i + 1, value=var_display_dict[var])
 
     # Enter info of listings
 
     row_number = 2
     # Loop over all listings
     for _, dict in scraped_dict.items():
-
         # Loop over all info
         for i in range(len(order)):
             var = order[i]
-            sheet.cell(row=row_number, column = i+1, value = dict[var])
+            sheet.cell(row=row_number, column=i + 1, value=dict[var])
 
         row_number += 1
 
+
 # Methods that filters the listings (can be changed as desired)
 def filter_listings(_, listing_dict, city):
-
-    if listing_dict["neighborhood"] in ['Tarwewijk', 
-                                        'Oosterflank', 
-                                        'Carnisse', 
-                                        'Zuidwijk', 
-                                        'Liskwartier', 
-                                        'Zestienhoven', 
-                                        'Bloemhof', 
-                                        'Lombardijen', 
-                                        'Hillegersberg',
-                                        'Oud Charlois',
-                                        'Groot IJsselmonde',
-                                        'Oud IJsselmonde',
-                                        'Zuidplein',
-                                        'Het Lage Land',
-                                        'Hillesluis',
-                                        'Feijenoord',
-                                        'Vreewijk']:
+    if listing_dict["neighborhood"] in [
+        "Tarwewijk",
+        "Oosterflank",
+        "Carnisse",
+        "Zuidwijk",
+        "Liskwartier",
+        "Zestienhoven",
+        "Bloemhof",
+        "Lombardijen",
+        "Hillegersberg",
+        "Oud Charlois",
+        "Groot IJsselmonde",
+        "Oud IJsselmonde",
+        "Zuidplein",
+        "Het Lage Land",
+        "Hillesluis",
+        "Feijenoord",
+        "Vreewijk",
+    ]:
         return False
     elif listing_dict["price_per_bedroom"] > 750:
-
-        if city == 'Amsterdam' and listing_dict["price_per_bedroom"] < 900:
+        if city == "Amsterdam" and listing_dict["price_per_bedroom"] < 900:
             return True
         else:
             return False
     else:
         return True
 
+
 # Dictionary that convert the variables to column names
-var_display_dict = {"price": "Price", 
-                 "area": "Surface Area", 
-                 "nrooms": "#Rooms", 
-                 "nbedrooms":"#Bedrooms", 
-                 "nbathrooms": "#Bathrooms", 
-                 "furnished":"Furnished", 
-                 "agent": "Agent", 
-                 "status": "Status",
-                 "zipcode": "Zipcode",
-                 "neighborhood": "Neighborhood",
-                 "street": "Street",
-                 "since": "Available since",
-                 "url": "Link",
-                 "price_per_bedroom": "Price per bedroom"}
+var_display_dict = {
+    "price": "Price",
+    "area": "Surface Area",
+    "nrooms": "#Rooms",
+    "nbedrooms": "#Bedrooms",
+    "nbathrooms": "#Bathrooms",
+    "furnished": "Furnished",
+    "agent": "Agent",
+    "status": "Status",
+    "zipcode": "Zipcode",
+    "neighborhood": "Neighborhood",
+    "street": "Street",
+    "since": "Available since",
+    "url": "Link",
+    "price_per_bedroom": "Price per bedroom",
+}
 
 # Order of the columns
-order = ['price', 'nbedrooms', 'nrooms', 'price_per_bedroom', 'area','neighborhood', 'street', 'zipcode', 'since', 'agent', 'url']
+order = [
+    "price",
+    "nbedrooms",
+    "nrooms",
+    "price_per_bedroom",
+    "area",
+    "neighborhood",
+    "street",
+    "zipcode",
+    "since",
+    "agent",
+    "url",
+]
